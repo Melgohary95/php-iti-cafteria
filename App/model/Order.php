bface04e6684f59c73404da140ef6285f6195527
mahmoudelgohary <mfelfel95@gmail.com>
2019-03-14 20:59:30 +0200

    modified routes and order model

--- App/model/Order.php
+++ App/model/Order.php
@@ -16,47 +16,11 @@ require '../../core/Database.php';
 class Order {
 
     //put your code here
-<<<<<<< HEAD
-    public $db;
-=======
     private $db;
->>>>>>> 3ecb882ebf7c1881827a54227f3324c831ba65c9
 
     public function __construct() {
         $this->db = new Database();
     }
-<<<<<<< HEAD
-
-    public function getOrders($fDate, $lDate) {
-        $myOrders = $this->db->select('orders', '*', "date between $fDate AND $lDate;
-        ");
-        return $myOrders;
-    }
-
-//-------------------- Aml ---------------------------
-    public function getOrdersByDate() {
-        if (isset($_POST['search'])) {
-            $startDate = $_POST['startDate'];
-            $endDate = $_POST['endDate'];
-            // $result = $this->db->select(table name, colm name, );
-            return $result;
-        }
-    }
-
-//-------------------- Aml ---------------------------
-//----------------fatma-----------
-//fatema
-
-    public function getAdminorders() {
-        $productOrders = array();
-        $orders = $this->db->select("orders", 'orders.date ,total_price ,rooms.number as roomNo,users.name as user_name,users.ext as                  ext', 'orders.user_id=users.id' and 'orders.room_id=rooms.id', null, null, "users,rooms");
-        $result['orders'] = $orders['resultset'];
-        var_dump($result['orders']);
-    }
-
-    /*     * ******************** nourhan *************************** */
-
-=======
     
     public function getOrders($fDate,$lDate){
             $myOrders;
@@ -98,7 +62,6 @@ class Order {
 
 //----------------fatma--------------------------//
     /*     * ******************** nourhan *************************** */
->>>>>>> 3ecb882ebf7c1881827a54227f3324c831ba65c9
     public function getAllProducts() {
         $productCategories = array();
         $categories = $this->db->select("categories");
@@ -130,11 +93,7 @@ class Order {
 
     public function addOrder($post_data) {
         $result = array();
-<<<<<<< HEAD
-        if ( isset($post_data['quantity']) && count($post_data['quantity']) > 0) {
-=======
         if (isset($post_data['quantity']) && count($post_data['quantity']) > 0) {
->>>>>>> 3ecb882ebf7c1881827a54227f3324c831ba65c9
             $data['status'] = 0;
             $data['room_id'] = $post_data['room_id'];
             $data['notes'] = $post_data['notes'];
@@ -143,32 +102,18 @@ class Order {
             $data['date'] = date('Y-m-d');
             $result = $this->db->insert("orders", $data);
             $quantities = $post_data['quantity'];
-<<<<<<< HEAD
-            foreach ($quantities as $key => $quantity)
-            {
-                $order_productData['order_id'] = $result['inserted_id'];
-                $order_productData['quantity'] =$quantity;
-=======
             foreach ($quantities as $key => $quantity) {
                 $order_productData['order_id'] = $result['inserted_id'];
                 $order_productData['quantity'] = $quantity;
->>>>>>> 3ecb882ebf7c1881827a54227f3324c831ba65c9
                 $order_productData['product_id'] = $post_data['products'][$key];
                 $order_productData['amount'] = $post_data['amount'][$key];
                 $this->db->insert("order_products", $order_productData);
             }
         }
     }
-<<<<<<< HEAD
-    
-    public function searchForProducts($valueToSearch)
-    {
-        $products = $this->db->select("products","*","name LIKE '%$valueToSearch%' and availability = 1 ");
-=======
 
     public function searchForProducts($valueToSearch) {
         $products = $this->db->select("products", "*", "name LIKE '%$valueToSearch%' and availability = 1 ");
->>>>>>> 3ecb882ebf7c1881827a54227f3324c831ba65c9
         $result = $products['resultset'];
         return $result;
     }
