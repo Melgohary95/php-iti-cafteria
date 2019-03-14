83db3681baf0be2a0e03863eafb9daec08af0537
nourhansherief <45966082+nourhansherief@users.noreply.github.com>
2019-03-14 19:26:37 +0200

    updating order model

--- App/model/Order.php
+++ App/model/Order.php
@@ -11,75 +11,111 @@
  *
  * @author lenovo
  */
-
-require '../../core/Database.php'; 
+require '../../core/Database.php';
 
 class Order {
+
     //put your code here
     private $db;
-    
+
     public function __construct() {
         $this->db = new Database();
     }
-    
-    public function getOrders($fDate,$lDate){
-            $myOrders;
-        if(!empty($fDate) && !empty($lDate)){
-            $myOrders = $this->db->select('orders','*',"date BETWEEN '$fDate' AND '$lDate';");
+
+    public function getOrders($fDate, $lDate) {
+        $myOrders;
+        if (!empty($fDate) && !empty($lDate)) {
+            $myOrders = $this->db->select('orders', '*', "date BETWEEN '$fDate' AND '$lDate';");
         } else {
-            $myOrders = $this->db->select('orders','*');
+            $myOrders = $this->db->select('orders', '*');
         }
         return $myOrders;
     }
+
     // public function getOrders($fDate, $lDate){
     //     $myOrders = $this->db->select('orders','*',"date between $fDate AND $lDate;");
     //     return $myOrders;
     // }
-
 //-------------------- Aml ---------------------------
 //-------------------- Aml ---------------------------
 //----------------fatma-----------
-public function getAdminorders() {
+    public function getAdminorders() {
         $productOrders = array();
-        $orders =$this->db->select("orders" ,
-        'orders.date ,orders.id,total_price ,rooms.number as roomNo,users.name as user_name,users.ext as ext',
-        'users.id=orders.user_id and rooms.id=orders.room_id and STATUS=0',null ,null ,"users,rooms");
+        $orders = $this->db->select("orders", 'orders.date ,orders.id,total_price ,rooms.number as roomNo,users.name as user_name,users.ext as ext', 'users.id=orders.user_id and rooms.id=orders.room_id and STATUS=0', null, null, "users,rooms");
         $result['orders'] = $orders['resultset'];
-       var_dump($result['orders']);
-       foreach($orders['resultset'] as $order){
-        $orderId=$order['id'];
-        $productOrders[$orderId]=$this->db->select("order_products"
-        ,'products.image as pimg ,order_products.amount as amount,order_products.Quantity as Qun,products.id as p_id'
-        ," order_products.order_id=$orderId and order_products.product_id=products.id "
-        ,null ,null,"orders ,products ")['resultset'];
-       $result['$productOrders'] = $productOrders;
-        var_dump($result);
-       return $result;
-                                      }
-                                    }
+//        var_dump($result['orders']);
+
+        foreach ($orders['resultset'] as $order) {
+            $orderId = $order['id'];
+            $productOrders[$orderId] = $this->db->select("order_products"
+                            , 'products.image as pimg ,order_products.amount as amount,order_products.Quantity as Qun,products.id as p_id'
+                            , " order_products.order_id=$orderId and order_products.product_id=products.id "
+                            , null, null, "orders ,products ")['resultset'];
+            
+//            var_dump($result);
+            
+        }
+        $result['$productOrders'] = $productOrders;
+        return $result;
+    }
+
 //----------------fatma--------------------------//
-    /********************** nourhan ****************************/
-                                      public function getAllProducts() {
+    /*     * ******************** nourhan *************************** */
+    public function getAllProducts() {
         $productCategories = array();
         $categories = $this->db->select("categories");
-        $result['users']= $this->db->select("users" ,"*","role_id=2")['resultset'];
-        
+        $result['users'] = $this->db->select("users", "*", "role_id=2")['resultset'];
+        $result['rooms'] = $this->db->select("rooms", "*")['resultset'];
+
         $result['categories'] = $categories['resultset'];
         foreach ($categories['resultset'] as $category) {
             $categoryId = $category['id'];
-            $productCategories[$categoryId]= $this->db->select("products", '*', "category_id=$categoryId", "`products`.`id`DESC")['resultset'];
+            $productCategories[$categoryId] = $this->db->select("products", '*', "category_id=$categoryId and availability=1", "`products`.`id`DESC")['resultset'];
         }
         $result['productCategories'] = $productCategories;
-        
+
 //        var_dump($result);
         return $result;
     }
-    public function getProductDetails($id)
-    {
-        $product =$this->db->selectById("products", '*', "id=$id");
+
+    public function getProductDetails($id) {
+        $product = $this->db->selectById("products", '*', "id=$id");
         $result = $product['resultset'];
         return $result;
     }
-    /************************ nourhan *************************/
 
+    public function getUserRoom($id) {
+        $user = $this->db->selectById("users", '*', "id=$id");
+        $result = $user['resultset'];
+        return $result;
+    }
+
+    public function addOrder($post_data) {
+        $result = array();
+        if (isset($post_data['quantity']) && count($post_data['quantity']) > 0) {
+            $data['status'] = 0;
+            $data['room_id'] = $post_data['room_id'];
+            $data['notes'] = $post_data['notes'];
+            $data['total_price'] = $post_data['total_price'];
+            $data['user_id'] = $post_data['user_id'];
+            $data['date'] = date('Y-m-d');
+            $result = $this->db->insert("orders", $data);
+            $quantities = $post_data['quantity'];
+            foreach ($quantities as $key => $quantity) {
+                $order_productData['order_id'] = $result['inserted_id'];
+                $order_productData['quantity'] = $quantity;
+                $order_productData['product_id'] = $post_data['products'][$key];
+                $order_productData['amount'] = $post_data['amount'][$key];
+                $this->db->insert("order_products", $order_productData);
+            }
+        }
+    }
+
+    public function searchForProducts($valueToSearch) {
+        $products = $this->db->select("products", "*", "name LIKE '%$valueToSearch%' and availability = 1 ");
+        $result = $products['resultset'];
+        return $result;
+    }
+
+    /*     * ********************** nourhan ************************ */
 }
