14dbad0a5e4cdf46399b3dfe170ce142b14bab11
aml <aml.fci@gmail.com>
2019-03-14 12:17:03 +0200

    render dates in myorders page

--- views/myOrders.php
+++ views/myOrders.php
@@ -1,23 +1,6 @@
-<?php include('./components/header.php') ?>
+<?php include('../../views/components/header.php') ?>
 
-<<<<<<< HEAD
-<div class="row main-header">
-    <!-- Header Navbar: style can be found in header.less -->
-    <div class="col-md-4 navbar" >
-        <ul class="nav tab-bar-icons">
-            <li>
-                <a href="/phpProject/home.php">
-                    <span class="menu-text">Home</span>
-                </a>
-            </li> 
-            <li class="flat-box waves-effect waves-block">
-                <a href="/phpProject/myOrders.php">
-                    <span class="menu-text"> My Orders </span>
-                </a>
-            </li>
-=======
-<?php include('./components/navBar.php') ?>
->>>>>>> d785a4235713261cbb31de2535449c8844b5a5da
+<?php include('../../views/components/navBar.php') ?>
 
 <div class="container">
 
@@ -58,7 +41,11 @@
           <!-- first section  -->
             <div class="ac">
               <input class="ac-input" id="ac-1" name="ac-1" type="checkbox" />
-              <label class="ac-label" for="ac-1">Amal Moustafa</label>
+              <label class="ac-label" for="ac-1">
+              <?php foreach($orders["resultset"] as $order): ?>
+                <?php echo $order["date"] ?>
+              <?php  endforeach;?>
+              </label>
 
               <article class="ac-text" style="position: absolute;top: 80vh;left: 50vw;">
                   <div class="ac-sub">
@@ -73,6 +60,7 @@
         <td>55 EGP</td>
         <td>Cancel</td>
       </tr>
+      
       <tr>
         <td>
           <!-- second section  -->
@@ -90,38 +78,16 @@
         </td>
         <td>Out for delivery</td>
         <td>20 EGP</td>
-        <td>1</td>
+        <td></td>
       </tr>
-      <tr>
-        <td>
-          <!-- third section  -->
-          <div class="ac">
-              <input class="ac-input" id="ac-6" name="ac-6" type="checkbox" />
-              <label class="ac-label" for="ac-6">Fatema mohamed</label>
-            
-              <article class="ac-text" style="position: absolute;top: 80vh;left: 50vw;">
-                <div class="ac-sub">
-                  <label for="ac-6">order date 3</label>
-                </div>
-              </article>
-          </div>
-          <!-- third section  -->
-        </td>
-        <td>Done</td>
-        <td>29 EGP</td>
-        <td>1</td>
-      </tr>
-      <tr>
-        <td colspan="4">
-          
-        </td>
-      </tr>
+     
     </tbody>
   </table>
 </div>
 
 
 <!-- start pagination  -->
+
 <nav aria-label="Page navigation example">
 <ul class="pagination pagination-circle pg-blue">
   <li class="page-item disabled"><a class="page-link">First</a></li>
@@ -147,4 +113,5 @@
 </nav>
 <!-- end pagination -->
 
-<?php include('./components/footer.php') ?>
+<?php include('../../views/components/footer.php') ?>
+
