681d7b98ae1676b85615c3b14ddcdf6a4aeb7c62
“fatemagit config --global user.name “fatema <fatemamo7amed@gmail.com>
2019-03-14 13:58:25 +0200

    modified:App/controller/orders.php

--- App/controller/orders.php
+++ App/controller/orders.php
@@ -4,6 +4,6 @@
 require '../model/Order.php'; 
 $model = new Order();
 $result=$model->getAdminorders();
-
+var_dump ($result);
 ?>
 <?php include('../../views/adminOrders.php') ?>
--- App/model/Order.php
+++ App/model/Order.php
@@ -38,16 +38,14 @@ class Order {
     }
 //-------------------- Aml ---------------------------
 //----------------fatma-----------
-//fatema
-
-    public function getAdminorders() {
+public function getAdminorders() {
         $productOrders = array();
-        $orders =$this->db->select("orders" ,'orders.date ,total_price ,rooms.number as roomNo,users.name as user_name,users.ext as                  ext','orders.user_id=users.id' and 'orders.room_id=rooms.id',null ,null ,"users,rooms");
+        $orders =$this->db->select("orders" ,'orders.date ,total_price ,rooms.number as roomNo,users.name as user_name,users.ext as ext','users.id=orders.user_id and rooms.id=orders.room_id and STATUS=0',null ,null ,"users,rooms");
         $result['orders'] = $orders['resultset'];
-       var_dump($result['orders']);
-
-                                      } 
-    
+       //var_dump($result['orders']);
+return $result;
+                                      }
+//----------------fatma--------------------------//
     /********************** nourhan ****************************/
                                       public function getAllProducts() {
         $productCategories = array();
